5aff5bcdcab1d5a7dc464c71e7e9feec3ad74903
Taz8du29 <13140104+Taz8du29@users.noreply.github.com>
2018-01-30 00:26:53 +0100

    Added a new "edit" menu Menu callback functions are not yet implemented
    New method getWindow is meant to be used inside callbacks, to return a
    pointer to the main window from any widget.

--- editor/menuEntries.h
+++ editor/menuEntries.h
@@ -35,9 +35,27 @@ public:
     // Placeholder callback
     static void foo(Fl_Widget*, void*);
 
-    // Callbacks for the different menu items
+    // File operation (open, save and export) callbacks
     static void cb_open(Fl_Widget* wdg, void*);
-    static void cb_save(Fl_Widget* wdg, void*);
+    static void cb_save(Fl_Widget* wdg, void* as);
+    static void cb_export(Fl_Widget* wdg, void* as);
+
+    // Edit menu callbacks
+    static void cb_undo(Fl_Widget* wdg, void*);
+    static void cb_redo(Fl_Widget* wdg, void*);
+    static void cb_rand(Fl_Widget* wdg, void*);
+
+    // Help menu callbacks
+    static void cb_help(Fl_Widget* wdg, void*);
+    static void cb_faq(Fl_Widget* wdg, void*);
+    static void cb_license(Fl_Widget* wdg, void*);
+    static void cb_about(Fl_Widget* wdg, void*);
+
+    // Miscellanous callbacks
+    static void cb_quit(Fl_Widget* wdg, void*);
+
+private:
+    static inline Application* mainWindow(Fl_Widget* wdg);
 };
 
 
@@ -45,18 +63,25 @@ public:
 /* MENU ITEMS TREE */
 
 const Fl_Menu_Item __menu[] = {
-    {"&File", 0, 0, 0, FL_SUBMENU},
-        {"&New",        FL_CTRL+'n',          Menu::foo,     0},
-        {"&Open",       FL_CTRL+'o',          Menu::cb_open, 0, FL_MENU_DIVIDER},
-        {"&Save",       FL_CTRL+'s',          Menu::cb_save, 0},
-        {"Save &As   ", FL_CTRL+FL_SHIFT+'s', Menu::cb_save, 0, FL_MENU_DIVIDER},
-        {"&Quit",       FL_CTRL+'q',          Menu::foo,     0},
+    {"&File", FL_ALT+'f', 0, 0, FL_SUBMENU},
+        {"&New",          FL_CTRL+'n',          Menu::foo,       0},
+        {"&Open",         FL_CTRL+'o',          Menu::cb_open,   0,            FL_MENU_DIVIDER},
+        {"&Save",         FL_CTRL+'s',          Menu::cb_save,   (void*) false},
+        {"Save &As",      FL_CTRL+FL_SHIFT+'s', Menu::cb_save,   (void*) true, FL_MENU_DIVIDER},
+        {"&Export",       FL_CTRL+'e',          Menu::cb_export, (void*) false},
+        {"E&xport As  ",  FL_CTRL+FL_SHIFT+'e', Menu::cb_export, (void*) true, FL_MENU_DIVIDER},
+        {"&Quit",         FL_CTRL+'q',          Menu::cb_quit,   0},
         {0},
-    {"&Help", 0, 0, 0, FL_SUBMENU},
-        {"Sho&w Help",            FL_F+1, Menu::foo, 0},
-        {"&FAQ",                  0,      Menu::foo, 0, FL_MENU_DIVIDER},
-        {"View &License",         0,      Menu::foo, 0, FL_MENU_DIVIDER},
-        {"&About matrixNecklace", 0,      Menu::foo, 0},
+    {"&Edit", FL_ALT+'e', 0, 0, FL_SUBMENU},
+        {"&Undo",      FL_CTRL+'z', Menu::cb_undo, 0},
+        {"Re&do",      FL_CTRL+'y', Menu::cb_redo, 0, FL_MENU_DIVIDER},
+        {"Randomi&ze", FL_CTRL+'r', Menu::cb_rand, 0},
+        {0},
+    {"&Help", FL_ALT+'h', 0, 0, FL_SUBMENU},
+        {"Sho&w Help",            FL_F+1, Menu::cb_help,    0},
+        {"&FAQ",                  0,      Menu::cb_faq,     0, FL_MENU_DIVIDER},
+        {"View &License",         0,      Menu::cb_license, 0, FL_MENU_DIVIDER},
+        {"&About matrixNecklace", 0,      Menu::cb_about,   0},
         {0}
 };
 
