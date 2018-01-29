708081620d23aac57532c87406963e72991dca9a
Taz8du29 <13140104+Taz8du29@users.noreply.github.com>
2018-01-30 00:23:30 +0100

    New style for menu bar and logger field

--- editor/loggerField.cpp
+++ editor/loggerField.cpp
@@ -18,12 +18,12 @@
 LoggerField::LoggerField(uint16_t x, uint16_t y, uint16_t w, uint16_t h) :
     Fl_Text_Display(x, y, w, h)
 {
-    this->buffer(this->logBuffer);
-
+    this->box(FL_NO_BOX);
     this->color(fl_rgb_color(0x33));
     this->textcolor(fl_rgb_color(0xE5));
     this->textsize(10);
 
+    this->buffer(this->logBuffer);
     this->writeln("Init Done.");
 }
 
